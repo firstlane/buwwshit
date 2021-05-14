67c7b4b24a5e5ca785ba64bb0460ee28a103834b
Thompson Lane <thompsonplane@gmail.com>
2021-05-13 19:18:03 -0500

    Can now output uwu.

--- src/index.js
+++ src/index.js
@@ -1,88 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-
-// class Square extends React.Component {
-//     render() {
-//       return (
-//         <button className="square"
-//                 onClick={() => this.props.onClick()}>
-//           {this.props.value}
-//         </button>
-//       );
-//     }
-//   }
-  
-//   class Board extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             squares: Array(9).fill(null),
-//         };
-//     }
-
-//     handleClick(i) {
-//         const squares = this.state.squares.slice();
-//         squares[i] = 'X';
-//         this.setState({squares: squares});
-//     }
-
-//     renderSquare(i) {
-//       return (<Square value={this.state.squares[i]}
-//                      onClick={() => this.handleClick(i)}
-//              />);
-//     }
-  
-//     render() {
-//       const status = 'Next player: X';
-  
-//       return (
-//         <div>
-//           <div className="status">{status}</div>
-//           <div className="board-row">
-//             {this.renderSquare(0)}
-//             {this.renderSquare(1)}
-//             {this.renderSquare(2)}
-//           </div>
-//           <div className="board-row">
-//             {this.renderSquare(3)}
-//             {this.renderSquare(4)}
-//             {this.renderSquare(5)}
-//           </div>
-//           <div className="board-row">
-//             {this.renderSquare(6)}
-//             {this.renderSquare(7)}
-//             {this.renderSquare(8)}
-//           </div>
-//         </div>
-//       );
-//     }
-//   }
-  
-//   class Game extends React.Component {
-//     render() {
-//       return (
-//         <div className="game">
-//           <div className="game-board">
-//             <Board />
-//           </div>
-//           <div className="game-info">
-//             <div>{/* status */}</div>
-//             <ol>{/* TODO */}</ol>
-//           </div>
-//         </div>
-//       );
-//     }
-//   }
-  
-//   // ========================================
-  
-//   ReactDOM.render(
-//     <Game />,
-//     document.getElementById('root')
-//   );
-  
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Uwuifier from 'uwuifier';
@@ -96,15 +11,13 @@ class InputBox extends React.Component {
     }
 
     updateText(newText) {
-        console.log("newText:");
-        console.log(newText);
         this.setState({text: newText});
         this.props.onChange(this.state.text);
     }
 
     render() {
         return (
-            <textarea onChange={(newText) => this.updateText(newText)} name="input-box" rows="20" cols="100">
+            <textarea onChange={(newText) => this.updateText(newText.target.value)} name="input-box" rows="20" cols="100">
                 {this.state.text}
             </textarea>
         );
@@ -112,12 +25,9 @@ class InputBox extends React.Component {
 }
 
 class OutputBox extends React.Component {
-//function OutputBox(props) {
     render() {
-        console.log("render OutputBox");
-        //console.log(this.props.text);
         return (
-            <textarea name="output-box" readOnly rows="20" cols="100">
+            <textarea key={this.props.text} name="output-box" readOnly rows="20" cols="100">
                 {this.props.text}
             </textarea>
         );
@@ -136,16 +46,6 @@ class App extends React.Component {
     buwwuify() {
         const uwu = new Uwuifier();
         this.setState({outputText: uwu.uwuifySentence(this.state.inputText)});
-        //this.state.outputText = uwu.uwuifySentence(this.state.inputText);
-        console.log(this.state.outputText);
-    }
-
-    setOutput(text) {
-
-    }
-
-    getInput(text) {
-        alert('getInput');
     }
 
     render() {
